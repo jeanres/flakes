c837832bf5a24cf01ecacaa73215a3c6611fcf42
Jeanre Swanepoel <jeanres@icloud.com>
2023-11-18 14:45:42 +0200

    add base ruby flake

---
+++ ruby/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gcc
+            ruby_3_0
+            libmysqlclient
+            nodejs
+            tree-sitter
+          ];
+        };
+      });
+}
